6c8f9993e466c6e7e7e8c1973ffe289b9997c9ff
ikowasz <dev.ikowasz@gmail.com>
2025-04-05 21:31:13 +0200

    logger added to fetcher

--- src/Service/Loader/Fetch.php
+++ src/Service/Loader/Fetch.php
@@ -2,10 +2,20 @@
 
 namespace App\Service\Loader;
 
+use Psr\Log\LoggerInterface;
+
+
 class Fetch
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    )
+    {}
+
+
     public function url(string $url): string
     {
+        $this->logger->info("Fetching URL: $url");
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
@@ -19,6 +29,7 @@ class Fetch
 
         curl_close($ch);
 
+        $this->logger->info("Done fetching!");
         return $response;
     }
 }
